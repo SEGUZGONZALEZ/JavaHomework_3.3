15a8808c15e9c2e4d52fe2d6e63bb58e619c0525
SergeyGuzenko <seguz@yandex.ru>
2022-06-23 12:16:15 +0300

    reformating

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,8 +1,8 @@
 public class CreditPaymentService {
 
     public int calculate(int loanAmount, double interestRate, int loanPeriod) {
-        double interestRateMonth = interestRate / (100*12);
-        double degrading = Math.pow((1+interestRateMonth), -loanPeriod);
+        double interestRateMonth = interestRate / (100 * 12);
+        double degrading = Math.pow((1 + interestRateMonth), -loanPeriod);
 
         return (int) (loanAmount * interestRateMonth / (1 - degrading));
 
